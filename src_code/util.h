6e3fbcfe70268213bbeaeca5dae3dca923e33d2f
yang_glb <caliyang97@gmail.com>
2021-03-16 14:09:44 +0800

    util.h添加注释

--- src_code/util.h
+++ src_code/util.h
@@ -2,9 +2,11 @@
 // Latest edit by TeeKee on 2017/7/23.
 //
 
+/*UTIL_H为预处理变量，防止头文件中的定义重复包含；util是utiliy的缩写；“H”指代“header”*/
 #ifndef UTIL_H
 #define UTIL_H
 
+//路径名长度
 #define PATHLEN 128
 #define LISTENQ 1024
 #define BUFLEN 8192
@@ -13,6 +15,7 @@
 #define TK_CONF_OK 0
 #define TK_CONF_ERROR -1
 
+//用MIN宏定义来代替min函数
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 typedef struct tk_conf{
